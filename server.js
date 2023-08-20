5887d2353f714ddc895fc1e04f2e208868a38166
shimna <chimmu.p@gmail.com>
2023-08-20 16:18:17 +1000

    added models User and Blog and created association between models

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const session = require('express-session');
-// const routes = require('./controllers');
+const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
 
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
