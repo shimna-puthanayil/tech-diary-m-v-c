28b658ec0a7d06dc6500857b2a176131bb83ce77
shimna <chimmu.p@gmail.com>
2023-08-21 23:50:46 +1000

    added model Comment and created association between models Comment, Blog and User

--- models/index.js
+++ models/index.js
@@ -1,13 +1,26 @@
 const User = require('./User');
 const Blog = require('./Blog');
-
+const Comment = require('./Comment');
 User.hasMany(Blog, {
   foreignKey: 'userId',
   onDelete: 'CASCADE',
 });
-
 Blog.belongsTo(User, {
   foreignKey: 'userId',
 });
+Blog.hasMany(Comment, {
+  foreignKey: 'blogId',
+  onDelete: 'CASCADE',
+});
+Comment.belongsTo(Blog, {
+  foreignKey: 'blogId',
+});
+User.hasMany(Comment, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE',
+});
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+});
 
-module.exports = { User, Blog };
+module.exports = { User, Blog, Comment };
